7b847c6e957ead813d4497ac44c17f182e392ac8
Josh Kelly <joshkelly@Joshs-MacBook-Air.local>
2012-01-07 20:35:55 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timedsales::Application.config.secret_token = 'b5cd89d1cf4a3430519ab38297368c78364d5b116f210dc72e64e4d48a101b40e86b7608b2b1777291b60eb1aee1d66304ae90cc7918e898ee560674253cbd58'
