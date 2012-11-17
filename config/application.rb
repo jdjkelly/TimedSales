07f009939dc0f510bb3c6c9da92f5941cf1737b7
Joshua Kelly <jdjkelly@gmail.com>
2012-11-17 14:28:30 -0500

    Removed API secret wtfff

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,6 @@ end
 module Timedsales
   class Application < Rails::Application
     
-    # Shopify API connection credentials:
-    config.shopify.api_key = "4559061db4498239230d30c853bc43d5"
-    config.shopify.secret = "85849150ff67c95ea9acc449ac9c1982"
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
